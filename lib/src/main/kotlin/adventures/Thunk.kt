6f33da13a18cfebb2f4bcec976a10c7e1ef4f809
James Santucci <james.santucci@gmail.com>
2023-01-18 17:28:16 -0700

    remove examples from Thunk.kt

--- lib/src/main/kotlin/adventures/Thunk.kt
+++ lib/src/main/kotlin/adventures/Thunk.kt
@@ -16,26 +16,3 @@ class Thunk<A>(val f: (u: Unit) -> A) {
 
     // etc.
 }
-
-object ExpensiveComputations {
-    fun doSomething1(x: Int, y: Int, z: Int): Unit {
-        val result = x + y + z
-        println(result)
-    }
-
-    fun doSomething2(x: Int, y: Int, z: Int): Unit {
-        val result = x + y - z
-        println(result)
-    }
-
-    fun doThings(x: Int, y: Int, z: Int) = {
-        doSomething1(x, y, z)
-        doSomething2(x, y, z)
-    }
-
-    fun doThingsThunked(x: Int, y: Int, z: Int) = {
-        val thunk1 = Thunk.delay(::doSomething1, x, y, z)
-        val thunk2 = Thunk.delay(::doSomething2, x, y, z)
-        val thunk3 = Thunk.delay(::println, "hello")
-    }
-}
